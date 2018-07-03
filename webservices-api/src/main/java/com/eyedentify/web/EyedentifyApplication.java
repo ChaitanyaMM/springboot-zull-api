b847fd4229e1f3c4e395a5335f63bbc8ba393de3
ChaitanyaMM <krishna.bodala@magikminds.com>
2018-07-03 18:34:40 +0530

    webservices-commity

--- webservices-api/src/main/java/com/eyedentify/web/EyedentifyApplication.java
+++ webservices-api/src/main/java/com/eyedentify/web/EyedentifyApplication.java
@@ -1,9 +1,13 @@
 package com.eyedentify.web;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,7 +16,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-//@Controller
+@Controller
+@ComponentScan(basePackages = "com")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+
 public class EyedentifyApplication {
 	
 	public static void main(String[] args) {
